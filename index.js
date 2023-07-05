9bd965d23c79fcfd8a7bd875bea9cf4d4f8f5d79
Shiny <shinymary.chandran@crystaldelta.com>
2023-07-05 17:04:57 +0530

    Changing-the-port-and-adding-start

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const port =4000; 
+const port =3000; 
 const app = express();
 
 app.get("/",(req,res) =>{
     res.send("This is the node project with sequelize")
 })
 app.listen(port, () =>{
-    console.log('Running on port 4000');
+    console.log('Running on port 3000');
 })
